ec16296afed3257733257dee2f9ccccbdac982e1
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2015-10-28 16:58:32 +0200

    Fixed application name, updated verstion number

--- mix.exs
+++ mix.exs
@@ -2,8 +2,8 @@ defmodule Stats.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :stats,
-     version: "0.0.1",
+    [app: :mix_info,
+     version: "0.2.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
