358ec478495f94865bc35facf69ed76f57310eca
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2015-11-01 15:41:06 +0200

    Updated version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stats.Mixfile do
 
   def project do
     [app: :mix_info,
-     version: "0.2.1",
+     version: "0.5.0",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
