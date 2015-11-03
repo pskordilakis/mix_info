b3ac998fd3c10be4b5cb1f8d1c02bc4c9842a0b8
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2015-11-03 12:11:13 +0200

    Updated mix.exs for hex publishing

--- mix.exs
+++ mix.exs
@@ -5,28 +5,29 @@ defmodule Stats.Mixfile do
     [app: :mix_info,
      version: "0.7.2",
      elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    A mix task that counts directories, files, lines of code, modules, functions etc and displays the results.
+    """
+  end
+
+  defp package do
+    files: ["lib", "mix.exs", "README.md", "LICENSE"],
+    maintainers: ["Panagiotis Skordilakis"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/pskordilakis/mix_info"}
   end
 end
